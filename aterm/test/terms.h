ee4bf95f0f92b63d8b586d5bfd73f3107c3425e8
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-03-05 14:34:52 +0100

    Rename aterm-2.6/ to aterm/

---
+++ aterm/test/terms.h
@@ -0,0 +1,20 @@
+/*
+ * Generated at Fri Mar  5 12:50:20 2021
+ */
+
+#ifndef __terms_dict_H
+#define __terms_dict_H
+
+#include <aterm2.h>
+
+#include <assert.h>
+
+extern AFun symbolA;
+extern AFun symbolB;
+
+extern ATerm patternA;
+extern ATerm patternB;
+
+extern void init_terms_dict();
+
+#endif /* __terms_dict_H */
