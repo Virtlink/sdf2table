102287220fb7ce47398b9745c43964c7e03779e1
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-03-06 15:22:05 +0100

    Improve gitignore

---
+++ aterm/test/terms.h
@@ -0,0 +1,20 @@
+/*
+ * Generated at Sat Mar  6 02:46:07 2021
+ */
+
+#ifndef __terms_dict_H
+#define __terms_dict_H
+
+#include <aterm2.h>
+
+#include <assert.h>
+
+extern AFun symbolA;
+extern AFun symbolB;
+
+extern ATerm patternA;
+extern ATerm patternB;
+
+extern void init_terms_dict();
+
+#endif /* __terms_dict_H */
