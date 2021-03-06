caae7175ab3d3ac0a8e2cc32619c7ecae8fd01c5
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-03-06 01:51:34 +0100

    Add macos-intel build script

---
+++ build-macos-intel.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o noclobber
+set -o nounset
+set -o xtrace
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mkdir -p "$DIR/dist/"
+
+cd "$DIR/aterm/"
+echo "Building ATerm library..."
+autoreconf -vfi
+./configure --prefix="$DIR/dist/"
+make
+make install
+cd "$DIR"
+
+export LD_LIBRARY_PATH="$DIR/dist/lib"
+export PKG_CONFIG_PATH="$DIR/dist/lib/pkgconfig"
+
+cd "$DIR/sdf2bundle/"
+echo "Building sdf2table..."
+autoreconf -vfi
+./configure \
+  CFLAGS="-std=gnu89 -fbracket-depth=512" \
+  --build=x86_64-apple-darwin \
+  --enable-static \
+  --disable-shared \
+  --prefix="$DIR/dist/" 
+make
+make install
+cd "$DIR"
+echo "Done!"
